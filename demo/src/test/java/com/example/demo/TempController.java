5cb43dd8f876897de75e18501ff9df456aab6999
mizzcereal <sangwookpen96@gmail.com>
2023-07-14 15:33:34 +0900

    mvc설명

--- demo/src/test/java/com/example/demo/TempController.java
+++ demo/src/test/java/com/example/demo/TempController.java
@@ -4,14 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+// Component - IoC에 등록될 객체라고 알림
 @Controller
 
 public class TempController {
 
+    // ~~Mapping 화면 주소를 설정하는 어노테이션
     @GetMapping("/hello") // 실행하기전에 붙혀주기
     public ModelAndView index() {
-        ModelAndView modelAndView = new ModelAndView();
 
+        // 화면에 출력될 화면과 데이터를 세팅하는 객체
+        ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("first");
 
         return modelAndView;
@@ -19,3 +22,5 @@ public class TempController {
     }
 
 }
+
+// 위의 코드들로 mvc 3개가 다모임
