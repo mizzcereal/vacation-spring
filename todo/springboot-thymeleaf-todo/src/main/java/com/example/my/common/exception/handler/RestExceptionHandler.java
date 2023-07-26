dc12c49e34fd0f631bd8d8b31cba509d7477bedc
mizzcereal <sangwookpen96@gmail.com>
2023-07-26 14:40:53 +0900

    Map을만들어서 에러메시지 표시

--- todo/springboot-thymeleaf-todo/src/main/java/com/example/my/common/exception/handler/RestExceptionHandler.java
+++ todo/springboot-thymeleaf-todo/src/main/java/com/example/my/common/exception/handler/RestExceptionHandler.java
@@ -1,7 +1,12 @@
 package com.example.my.common.exception.handler;
 
+
+import java.util.HashMap;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -10,6 +15,28 @@ import com.example.my.common.exception.BadRequestException;
 
 @RestControllerAdvice // handler를 만들시 @RestControllerAdvice 사용
 public class RestExceptionHandler {
+
+    @ExceptionHandler(BindException.class)
+    public ResponseEntity<?> handleBindException(BindException bindException){
+        HashMap<String, String> errMap = new HashMap<>();
+
+
+        bindException.getBindingResult().getFieldErrors(); //여러에러들이 getFieldErrors안에 들어있다.
+
+        for (FieldError fieldError : bindException.getBindingResult().getFieldErrors()) {
+            errMap.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+
+        return new ResponseEntity<>(
+            ResponseDTO.builder()
+            .code(1)
+            .message("요청 데이터를 확인해주세요")
+            .data(errMap)
+            .build(),
+            HttpStatus.BAD_REQUEST
+        );
+    }
+
 
     // RestController니까 ResponseEntity 사용
     @ExceptionHandler(BadRequestException.class) // BadRequestException 발생시 아래함수를 실행
