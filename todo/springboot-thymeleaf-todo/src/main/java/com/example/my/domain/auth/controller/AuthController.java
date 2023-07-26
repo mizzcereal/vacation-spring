084fbaca3e7396749f2d7410f719f11c0fa2de9f
mizzcereal <sangwookpen96@gmail.com>
2023-07-26 10:29:19 +0900

    Controller-ModelAndView

--- todo/springboot-thymeleaf-todo/src/main/java/com/example/my/domain/auth/controller/AuthController.java
+++ todo/springboot-thymeleaf-todo/src/main/java/com/example/my/domain/auth/controller/AuthController.java
@@ -3,32 +3,32 @@ package com.example.my.domain.auth.controller;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.servlet.ModelAndView;
+
+
 
 @Controller
 public class AuthController {
 
+    // @GetMapping("/auth/login")
+    // public ModelAndView login() {
+    // ModelAndView modelAndView = new ModelAndView();
+    // modelAndView.setViewName("auth/login");
+    // return modelAndView;
+    // }
+
     @GetMapping("/auth/login")
-    public ModelAndView login() {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("auth/login");
-        return modelAndView;
-    }
+    public String login() {
+        return "auth/login";
+    } // auth/login 은 viewname이라 위에 주석처리 한 코드와 같은 동작방식으로 처리한다.
 
     @GetMapping("/auth/join")
-    public ModelAndView join() {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("auth/join");
-        return modelAndView;
+    public String join() {
+        return "auth/join";
     }
 
     @GetMapping("/auth/logout")
-    public ModelAndView logout(HttpSession session) {
-
+    public String logout(HttpSession session) {
         session.invalidate(); // session안에 있는 데이터를 모두 날리겠다(로그아웃)
-
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("redirect:/auth/login"); // redirect: 페이지 - 그 페이지로 이동하겠다.
-        return modelAndView;
+        return "auth/logout";
     }
 }
